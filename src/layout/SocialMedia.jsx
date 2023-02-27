dcd6ddb5798ada32029cd29bb7c9ef9e10dc0ba1
Juan Fonseca <110582471+juanfonseca09@users.noreply.github.com>
2023-02-27 17:15:08 -0300

    Update SocialMedia.jsx

--- src/layout/SocialMedia.jsx
+++ src/layout/SocialMedia.jsx
@@ -17,7 +17,7 @@ export const SocialMedia = () => {
         <div className="home-hero__social">
           <a href="https://github.com/juanfonseca09" className="home-hero__social-icon-link" target="_blank">
             <img
-              src="./png/github-ico.png"
+              src="/png/github-ico.png"
               alt="icon"
               className="home-hero__social-icon"
               rel="noreferrer"
