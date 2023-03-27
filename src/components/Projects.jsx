2d0758596cc4b5cc74eb4cb12986331c53a0b3dc
Juan Fonseca <110582471+juanfonseca09@users.noreply.github.com>
2023-03-27 20:26:22 -0300

    change2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,17 +4,18 @@ import { trabajos } from '../data/trabajos'
 
 export const Projects = () => {
   return (
-    <motion.div inicial={{opacity:0,}} animate={{opacity:1}} exit={{opacity:0}} transition={{duration:0.3}}>
+
+    <motion.div className="home-hero" inicial={{opacity:0,}} animate={{opacity:1}} exit={{opacity:0}} transition={{duration:0.3}}>
       <section className='works'>
       {
         trabajos.map(trabajo => {
           return (
             <article className='work-item' key={trabajo.id}>
               <div className='mask'>
-                <img href={trabajo.url} target='_blank' src={'/png/'+trabajo.id+'.png'}/>
+                <a className='ahov' type='button' href={trabajo.url} target='_blank'><img src={'/png/'+trabajo.id+'.png'}/></a>
               </div>
               <span>{trabajos.categorias}</span>
-              <h2 href={trabajo.url} target='_blank'>{trabajo.nombre}</h2>
+              <a type='button' href={trabajo.url} target='_blank'><h2>{trabajo.nombre}</h2></a>
               <h3>{trabajo.tecnologias}</h3>
             </article>
           )
